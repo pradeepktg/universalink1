815492bf07e8ba4c2e8006415778745d1c7b332d
Pradeep <pradeep.ktg@gmail.com>
2021-05-12 15:05:16 +0530

    AASA in root

--- app.py
+++ app.py
@@ -6,11 +6,22 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/.well-known")
 def index():
-    return "<h1>Hello World!</h1>"
+    return "<h1>Welcome to universal link home page</h1>"
+
+@app.route("/reservation/")
+def reservation():
+    return "<h1>Reservation Screen</h1>"
+
+@app.route("/content/")
+def content():
+    return "<h1>Content Screen</h1>"
 
 @app.route("/.well-known/<path:filename>", methods=['GET','POST'])
 def wellknown(filename):
-
-                           return send_from_directory(directory=app.root_path + '/well-known/',
+    return send_from_directory(directory=app.root_path + '/well-known/',
                                                   filename=filename,
                                                   mimetype='application/json')
+
+@app.route("/<path:filename>", methods=['GET','POST'])
+def aasa(filename):
+    return send_from_directory(directory=app.root_path + '/well-known/',filename=filename,mimetype='application/json')
